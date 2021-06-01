0ba97e562ac3142f7f88bba0ff7cc25f64411ca4
Elton Lobo <elton1231@gmail.com>
2021-06-01 13:42:50 +0400

    feat: export report

--- renovation_core_graphql/reports/__init__.py
+++ renovation_core_graphql/reports/__init__.py
@@ -1,7 +1,10 @@
 from graphql import GraphQLSchema
-from .resolvers import get_renovation_report_resolver
+from .resolvers import get_renovation_report_resolver, \
+    export_renovation_report_resolver
 
 
 def bind_schema(schema: GraphQLSchema):
     schema.query_type.fields[
         "getRenovationReport"].resolve = get_renovation_report_resolver
+    schema.query_type.fields[
+        "exportRenovationReport"].resolve = export_renovation_report_resolver
