37c4c1c6879e564b371488d4aa0f925551343841
Elton Lobo <elton1231@gmail.com>
2021-06-02 18:09:10 +0400

    fix: remove hook declaration

--- renovation_core_graphql/hooks.py
+++ renovation_core_graphql/hooks.py
@@ -16,7 +16,6 @@ graphql_sdl_dir = [
     "./renovation_core_graphql/renovation_core_graphql/model/sdls",
     "./renovation_core_graphql/renovation_core_graphql/notifications/sdls",
     "./renovation_core_graphql/renovation_core_graphql/translation/types",
-    "./renovation_core_graphql/renovation_core_graphql/reports/sdls",
     "./renovation_core_graphql/renovation_core_graphql/utils/types",
 ]
 
@@ -25,7 +24,6 @@ graphql_schema_processors = [
     "renovation_core_graphql.model.bind_schema",
     "renovation_core_graphql.notifications.bind_schema",
     "renovation_core_graphql.translation.bind_schema",
-    "renovation_core_graphql.reports.bind_schema",
     "renovation_core_graphql.utils.bind_schema"
 ]
 
