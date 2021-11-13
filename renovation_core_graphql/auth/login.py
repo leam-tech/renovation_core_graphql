06299c66164f4bb7e968ac0b56e74b119006089d
Fahim Ali Zain <fahimalizain@gmail.com>
2021-11-13 09:20:18 +0530

    fix: verbose error message for login

--- renovation_core_graphql/auth/login.py
+++ renovation_core_graphql/auth/login.py
@@ -1,4 +1,4 @@
-from graphql import GraphQLResolveInfo
+from graphql import GraphQLResolveInfo, GraphQLError
 
 import frappe
 from frappe.auth import LoginManager
@@ -13,12 +13,17 @@ def login_resolver(obj, info: GraphQLResolveInfo, **kwargs):
     frappe.local.form_dict.use_jwt = 1 if use_jwt else 0
 
     login = LoginManager()
-    login.authenticate(user, pwd)
-    login.post_login()
-    login.run_trigger('on_session_creation')
+    try:
+        login.authenticate(user, pwd)
+        login.post_login()
+        login.run_trigger('on_session_creation')
 
-    obj = frappe.local.response
-    obj["user"] = frappe._dict(doctype="User", name=obj["user"])
+        obj = frappe.local.response
+        obj["user"] = frappe._dict(doctype="User", name=obj["user"])
+    except frappe.AuthenticationError as e:
+        raise GraphQLError("Invalid Credentials", original_error=e)
+    except Exception as e:
+        raise GraphQLError("Something went wrong", original_error=e)
 
     return obj
 
