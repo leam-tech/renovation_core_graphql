0470d6905659bbb414e9f12154ec521434a3a44f
Elton Lobo <41537985+e-lobo@users.noreply.github.com>
2022-01-23 16:52:58 +0400

    feat: export data mutation (#12)
    * feat: export data mutation
    
    * refactor: use different Exporter

--- renovation_core_graphql/utils/__init__.py
+++ renovation_core_graphql/utils/__init__.py
@@ -1,7 +1,8 @@
 from graphql import GraphQLSchema
-from renovation_core_graphql.utils.resolvers import search_link_resolver
+from renovation_core_graphql.utils.resolvers import search_link_resolver, export_data_resolver
 
 
 def bind_schema(schema: GraphQLSchema):
     schema.query_type.fields[
         "search_link"].resolve = search_link_resolver
+    schema.query_type.fields["export_data"].resolve = export_data_resolver
