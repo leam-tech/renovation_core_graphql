cc0b56fa10a6e07ad4daa641fac2e8d153ce53a8
Fahim Ali Zain <fahimalizain@gmail.com>
2022-08-22 13:12:51 +0530

    Revert "fix: Updated ctx-translation resolvers (#14)"
    This reverts commit fd747267170bdaf93a37642cd717ac2fa140b2ed.

---
+++ renovation_core_graphql/document_resolver.py
@@ -0,0 +1,27 @@
+from frappe.utils import cint
+from graphql import GraphQLResolveInfo
+
+import frappe
+from frappe_graphql.utils.resolver import document_resolver, get_singular_doctype
+from renovation_core.utils.translate import get_ctx_translation
+
+
+def resolve(obj, info: GraphQLResolveInfo, **kwargs):
+    # We are here just to implement our custom translation
+    frappe.flags.ignore_doc_resolver_translation = True
+    value = document_resolver(
+        obj=obj, info=info, **kwargs
+    )
+    doctype = obj.get('doctype') or get_singular_doctype(info.parent_type.name)
+    meta = frappe.get_meta(doctype)
+    df = meta.get_field(info.field_name)
+
+    if df and cint(df.get("translatable")):
+        try:
+            cached_doc = frappe.get_cached_doc(doctype, obj.get("name"))
+        except BaseException:
+            cached_doc = obj
+
+        return get_ctx_translation(
+            doc=cached_doc, fieldname=info.field_name, value=value)
+    return value
