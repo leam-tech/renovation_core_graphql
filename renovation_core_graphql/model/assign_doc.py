e33773a1dbedc11c2c842fc5e81b03de2068f0fe
Fahim Ali Zain <fahimalizain@gmail.com>
2023-01-14 17:45:12 +0530

    fix: Updated ctx-translation resolvers + Dataloader Fixes (#17)
    * fix: Updated ctx-translation resolvers
    
    * fix: GQL Dataloader support for renovation_core_graphql

--- renovation_core_graphql/model/assign_doc.py
+++ renovation_core_graphql/model/assign_doc.py
@@ -16,8 +16,8 @@ def get_docs_assigned_to_user_resolver(obj, info: GraphQLResolveInfo, **kwargs):
         todo.due_date = todo.dueDate
         todo.reference_type = todo.doctype
         todo.reference_name = todo.docname
-        todo.assigned_by = frappe._dict(doctype="User", name=todo.assignedBy)
-        todo.assigned_to = frappe._dict(doctype="User", name=todo.assignedTo)
+        todo.assigned_by = _get_user_for_gql(todo.assignedBy)
+        todo.assigned_to = _get_user_for_gql(todo.assignedTo)
 
     return r
 
@@ -32,8 +32,8 @@ def get_users_assigned_to_doc_resolver(obj, info: GraphQLResolveInfo, **kwargs):
         todo.due_date = todo.dueDate
         todo.reference_type = kwargs.get("doctype")
         todo.reference_name = kwargs.get("name")
-        todo.assigned_by = frappe._dict(doctype="User", name=todo.assignedBy)
-        todo.assigned_to = frappe._dict(doctype="User", name=todo.assignedTo)
+        todo.assigned_by = _get_user_for_gql(todo.assignedBy)
+        todo.assigned_to = _get_user_for_gql(todo.assignedTo)
 
     return r
 
@@ -52,8 +52,8 @@ def assign_doc_resolver(obj, info: GraphQLResolveInfo, **kwargs):
         todo.name = None
         todo.doctype = None
         todo.due_date = todo.date
-        todo.assigned_to = frappe._dict(doctype="User", name=todo.owner)
-        todo.assigned_by = frappe._dict(doctype="User", name=todo.assigned_by)
+        todo.assigned_to = _get_user_for_gql(todo.owner)
+        todo.assigned_by = _get_user_for_gql(todo.assigned_by)
 
     return r
 
@@ -70,8 +70,8 @@ def unassign_doc_resolver(obj, info: GraphQLResolveInfo, **kwargs):
         todo.name = None
         todo.doctype = None
         todo.due_date = todo.date
-        todo.assigned_to = frappe._dict(doctype="User", name=todo.owner)
-        todo.assigned_by = frappe._dict(doctype="User", name=todo.assigned_by)
+        todo.assigned_to = _get_user_for_gql(name=todo.owner)
+        todo.assigned_by = _get_user_for_gql(name=todo.assigned_by)
 
     return r
 
@@ -89,7 +89,14 @@ def set_assignment_status_resolver(obj, info: GraphQLResolveInfo, **kwargs):
         todo.name = None
         todo.doctype = None
         todo.due_date = todo.date
-        todo.assigned_to = frappe._dict(doctype="User", name=todo.owner)
-        todo.assigned_by = frappe._dict(doctype="User", name=todo.assigned_by)
+        todo.assigned_to = _get_user_for_gql(todo.owner)
+        todo.assigned_by = _get_user_for_gql(todo.assigned_by)
 
     return r
+
+
+def _get_user_for_gql(user: str):
+    user_doc = frappe.get_doc("User", user)
+    user_doc.apply_fieldlevel_read_permissions()
+
+    return user_doc.as_dict(convert_dates_to_str=True)
